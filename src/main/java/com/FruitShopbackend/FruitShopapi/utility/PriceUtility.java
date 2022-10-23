e36a842da548b92e62575a9439c23e506f6d97ca
Filip Kelemen <filip.kelemen@dialogdata.de>
2022-10-23 17:11:43 +0300

    Refactored price utility

--- src/main/java/com/FruitShopbackend/FruitShopapi/utility/PriceUtility.java
+++ src/main/java/com/FruitShopbackend/FruitShopapi/utility/PriceUtility.java
@@ -4,18 +4,20 @@ public class PriceUtility {
 
     private PriceUtility(){}
 
-    public static String getFormattedPrice(final long priceValue,final String currency) throws Exception {
+    public static String getFormattedPrice(final long priceValue,final String currency) {
         final int NUMBER_OF_DECIMALS = 2;
         final String stringifiedPrice = Long.toString(priceValue);
-        if(stringifiedPrice.length() < NUMBER_OF_DECIMALS) {
-            throw new Exception("Value not big enough to handle " + NUMBER_OF_DECIMALS + " decimals\n");
+        String numberOfCurrencyUnits = "0";
+        String decimalPart = "0";
+        //this handles 0.123...;0.0000...123
+        if(stringifiedPrice.length() <= NUMBER_OF_DECIMALS) {
+            decimalPart = "0".repeat(NUMBER_OF_DECIMALS - stringifiedPrice.length()) + stringifiedPrice;
         }
-        String numberOfCurrencyUnits = stringifiedPrice.substring(0,stringifiedPrice.length() - NUMBER_OF_DECIMALS);
-        //handling case where price should be 0.123...
-        if(numberOfCurrencyUnits.length() == 0) {
-            numberOfCurrencyUnits = "0";
+        //this handles at least one currency unit
+        if(stringifiedPrice.length() > NUMBER_OF_DECIMALS){
+            numberOfCurrencyUnits = stringifiedPrice.substring(0,stringifiedPrice.length() - NUMBER_OF_DECIMALS);
+            decimalPart = stringifiedPrice.substring(stringifiedPrice.length() - NUMBER_OF_DECIMALS);
         }
-        final String decimalPart = stringifiedPrice.substring(stringifiedPrice.length() - NUMBER_OF_DECIMALS);
         return currency + numberOfCurrencyUnits + ',' + decimalPart;
     }
 }
